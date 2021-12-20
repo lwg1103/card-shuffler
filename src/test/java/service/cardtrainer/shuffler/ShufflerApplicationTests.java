60b1cf7da3e7f094c02b11f664d85fbb3ead9b64
lwg1103 <lwg1103@wp.pl>
2021-12-20 11:34:18 +0100

    init commit

---
+++ src/test/java/service/cardtrainer/shuffler/ShufflerApplicationTests.java
@@ -0,0 +1,13 @@
+package service.cardtrainer.shuffler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShufflerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
